c4ccfa08dd9eb030bb4388360ec795ce68d7e34d
Francois <francois.thullier98@gmail.com>
2024-11-15 08:55:50 +0100

    Removed the displayDefault subfunction in the DisplayDefaultProject function, caused a bug when removing all projects and adding a new one

--- src/display-project.js
+++ src/display-project.js
@@ -1,75 +1,67 @@
-const DisplayProjectName=(name) => {
+const DisplayProjectName = (name) => {
+  const UpdateHeaderTitle = (projectName) => {
+    const headerTitle = document.querySelector(".header h1");
+    headerTitle.textContent = `Project : ${projectName}`;
+  };
 
-    const UpdateHeaderTitle=(projectName) => {
+  const UpdateMainContainerTitle = (projectName, projectContainer) => {
+    let containerTitleText = projectContainer.querySelector("h3");
 
-        const headerTitle=document.querySelector(".header h1");
-        headerTitle.textContent = `Project : ${projectName}`;
+    if (!containerTitleText) {
+      containerTitleText = document.createElement("h3");
+      projectContainer.appendChild(containerTitleText);
+    }
 
-    };
-    
-    const UpdateMainContainerTitle=(projectName,projectContainer)=> {
+    containerTitleText.textContent = `You are currently working on ${projectName} project`;
+  };
 
-        let containerTitleText = projectContainer.querySelector('h3');
-        
-        if (!containerTitleText) {
-            containerTitleText = document.createElement("h3");
-            projectContainer.appendChild(containerTitleText);
-            }
-        
-                   
-        containerTitleText.textContent = `You are currently working on ${projectName} project`;
+  const DisplayDefaultProject = (defaultProjectName) => {
+    const defaultProjectContainer = document.querySelector(
+      `.main-container #${defaultProjectName}`
+    );
+    defaultProjectContainer.style.display = "grid"; // Set display to 'grid' to show it
+    defaultProjectContainer.style.visibility = "visible";
 
-    };
+    UpdateMainContainerTitle(
+      defaultProjectContainer.id,
+      defaultProjectContainer
+    );
+    UpdateHeaderTitle(defaultProjectContainer.id);
+  };
 
-    const DisplayDefaultProject=(defaultProjectName) => {
+  // DisplayDefaultProject("Default");
 
-        const defaultProjectContainer=document.querySelector(`.main-container #${defaultProjectName}`);
-        defaultProjectContainer.style.display = 'grid'; // Set display to 'grid' to show it
-        defaultProjectContainer.style.visibility="visible";
+  const HandleClickSidebarProject = (() => {
+    const SidebarprojectsContainer = document.querySelectorAll(
+      ".sidebar-projects button"
+    );
 
-        UpdateMainContainerTitle(defaultProjectContainer.id,defaultProjectContainer);
-        UpdateHeaderTitle(defaultProjectContainer.id);
+    // display Default project when loading the page
 
-    };
+    SidebarprojectsContainer.forEach((elem) => {
+      //when the sidebar button of the project is click, DOM should be added to mainContainer
+      elem.addEventListener("click", () => {
+        document.querySelectorAll(".main-container .project").forEach((div) => {
+          div.style.display = "none";
+        });
+        let projectMainContainer = document.querySelector(
+          `.main-container #${elem.id}`
+        );
 
-    DisplayDefaultProject("Default");
+        projectMainContainer.style.display = "grid"; // Set display to 'grid' to show it
+        projectMainContainer.style.visibility = "visible";
 
-    
+        //function call
 
-    const HandleClickSidebarProject=(() => {
-        const SidebarprojectsContainer=document.querySelectorAll(".sidebar-projects button");
+        UpdateMainContainerTitle(elem.id, projectMainContainer);
+        UpdateHeaderTitle(elem.id);
 
-        // display Default project when loading the page
+        //
+      });
+    });
+  })();
 
-        
-        SidebarprojectsContainer.forEach((elem) => {  //when the sidebar button of the project is click, DOM should be added to mainContainer
-            elem.addEventListener("click",() => {
-                document.querySelectorAll('.main-container .project').forEach(div => {
-                    div.style.display = 'none'; 
-                });
-                let projectMainContainer = document.querySelector(`.main-container #${elem.id}`);
-               
-    
-                projectMainContainer.style.display = 'grid'; // Set display to 'grid' to show it
-                projectMainContainer.style.visibility="visible";
-    
-                //function call
-                
-                UpdateMainContainerTitle(elem.id,projectMainContainer);
-                UpdateHeaderTitle(elem.id);
-    
-                //
-            }
-                
-            )
-            
-        })
+  return name;
+};
 
-
-    })();
-
-    
-    return(name);
-}
-
-export  {DisplayProjectName} ;
+export { DisplayProjectName };
--- src/index.js
+++ src/index.js
@@ -1,33 +1,23 @@
-import {CreateProject,deleteProject} from "./create-project.js";
+import { CreateProject, deleteProject } from "./create-project.js";
 import { getProjectName } from "./get-project-name.js";
 import { DisplayProjectName } from "./display-project.js";
 import { formPopup } from "./todo-form-popup.js";
-import { compareAsc, format } from "date-fns";
 import "./styles.css";
 
+const handleClick = (() => {
+  DisplayProjectName(CreateProject("Default")); //creates a default project with the name "Default"
+  deleteProject();
+  let projectIdArray = ["Default"];
 
-
-const handleClick=(() => {   
-
-    DisplayProjectName(CreateProject("Default")); //creates a default project with the name "Default"
+  formPopup(projectIdArray[0]); //creates a project in the DOM and display it
+  const createProjectButton = document.querySelector(
+    "#sidebar-create-project-button"
+  );
+  createProjectButton.addEventListener("click", () => {
+    let projectName = getProjectName();
+    projectIdArray.push(CreateProject(projectName));
     deleteProject();
-    let projectIdArray=["Default"];
-
-    formPopup(projectIdArray[0]);                           //creates a project in the DOM and display it
-    const createProjectButton=document.querySelector("#sidebar-create-project-button");
-    createProjectButton.addEventListener("click", () => {
-        
-
-        let projectName=getProjectName();
-        projectIdArray.push((CreateProject(projectName)));
-        deleteProject();
-        DisplayProjectName();
-        formPopup(projectName);
-
-
-        
-        
-    })
+    DisplayProjectName();
+    formPopup(projectName);
+  });
 })();
-
-
